0bee7ba5933ae495e74d504976bcbbbfc64e84b7
cisgenderman <grigorjevsergey1@gmail.com>
2023-01-07 17:10:22 +0700

    zsdf

--- Compression_With_Arithmetic_Coding/Compression_With_Arithmetic_Coding.cpp
+++ Compression_With_Arithmetic_Coding/Compression_With_Arithmetic_Coding.cpp
@@ -3,4 +3,30 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
+	char* String = new char[1000];
+	float encoding_message_number;
+
+	//считывание файла
+	FILE* stream;
+	errno_t Input = fopen_s(&stream, "Input.txt", "r");
+	char x;
+	int i = 0;
+	while ((feof(stream) == 0))
+	{
+		fscanf_s(stream, "%c", &x);
+		String[i] = x;
+		i++;
+	}
+	String[i - 1] = '\0';
+	fclose(stream);
+
+	///////////////////////////////////
+	
+	//вывод в файл
+	errno_t Output = fopen_s(&stream, "Output.txt", "w");
+	fprintf(stream, "number encoding message: %d\n", encoding_message_number);
+	fclose(stream);
+	delete[] String;
+	return 0;
 }
