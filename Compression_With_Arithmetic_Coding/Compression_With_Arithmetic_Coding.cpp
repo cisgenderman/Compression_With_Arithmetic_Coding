e3813419b93f39c7c7719b2a65a125a86a9493fa
cisgenderman <grigorjevsergey1@gmail.com>
2023-01-07 19:54:26 +0700

    wqe

--- Compression_With_Arithmetic_Coding/Compression_With_Arithmetic_Coding.cpp
+++ Compression_With_Arithmetic_Coding/Compression_With_Arithmetic_Coding.cpp
@@ -1,5 +1,27 @@
 ﻿#include <iostream>
+#include <errno.h>
+
+using namespace std;
+
+struct sym
+{
+	unsigned char ch;
+	float freq;
+	char code[255];
+	sym* left;
+	sym* right;
+};
+
+int chh;					//переменная для подсчета информация из строки
+int k = 0;					//счётчик количества различных букв, уникальных символов
+int kk = 0;					//счетчик количества всех знаков в файле
+int kolvo[256] = { 0 };		//инициализируем массив количества уникальных символов
+sym simbols[256] = { 0 };	//инициализируем массив записей
+sym* psym[256];				//инициализируем массив указателей на записи
+float summ_of_all_freq = 0;	//сумма частот встречаемости
+
 //compression
+/*
 int l_0 = 0;
 int h_0 = 65535;
 int i = 0;
@@ -45,8 +67,9 @@ void BitsPlusFollow(int bit)
 		CompressedFile.WriteBit(!bit);
 	}
 }
-
+*/
 //decompression
+/*
 int l[0] = 0;
 int h[0] = 65535;
 float delitel = b[c_last];
@@ -83,7 +106,71 @@ for (i = 1; i < compressedFile.DataLength(); i++)
 	}
 	DataFile.WriteSymbol(c);
 };
+*/
 
+void Statistics(char* String)
+{
+	//посимвольно считываем строку и составляем таблицу встречаемости
+	for (int i = 0; i < strlen(String); i++)
+	{
+		//цикл для подсчета информация из строки
+		chh = String[i];
+		for (int j = 0; j < 256; j++)
+		{
+			//если символ нашли в массиве записей символов, то в массиве количества уникальных символов увеличиваем количество
+			//и увеличиваем общее количество символов
+			if (chh == simbols[j].ch)
+			{
+				kolvo[j]++;
+				kk++;
+				break;
+			}
+			//если не нашли в массиве записей символов, то знаносим этот символ 
+			//в массиве количества уникальных символов ставим единицу
+			//и увеличиваем общее количество символов и уникальных символов
+			if (simbols[j].ch == 0)
+			{
+				simbols[j].ch = (unsigned char)chh;
+				kolvo[j] = 1;
+				k++;
+				kk++;
+				break;
+			}
+		}
+	}
+	// расчет частоты встречаемости
+	for (int i = 0; i < k; i++)
+	{
+		simbols[i].freq = (float)kolvo[i] / kk;
+	}
+	// в массив указателей заносим адреса записей
+	for (int i = 0; i < k; i++)
+	{
+		psym[i] = &simbols[i];
+	}
+	//сортировка по убыванию
+	sym tempp;
+	for (int i = 1; i < k; i++)
+	{
+		for (int j = 0; j < k - 1; j++)
+		{
+			if (simbols[j].freq < simbols[j + 1].freq)
+			{
+				tempp = simbols[j];
+				simbols[j] = simbols[j + 1];
+				simbols[j + 1] = tempp;
+			}
+		}
+	}
+	//печатаем статистику 
+	//по итогу сумма частот должна дать 1
+	for (int i = 0; i < k; i++)
+	{
+		summ_of_all_freq += simbols[i].freq;
+		printf("Character = %d\tFrequancy = %f\tSymbol = %c\t\n", simbols[i].ch, simbols[i].freq, psym[i]->ch);
+	}
+	printf("\nKolovo simvolov : %d\nSumm of all Frequancy : %f\n", kk, summ_of_all_freq);
+}
 int main()
 {
     std::cout << "Hello World!\n";
@@ -104,6 +191,8 @@ int main()
 	}
 	String[i - 1] = '\0';
 	fclose(stream);
+	////////////////////////////////////////
+	Statistics(String);						//вызов функции определения частоты символов в строке
 
 	///////////////////////////////////
 	
